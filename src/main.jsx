2288dd456921ec809156c1c61a91b006f4b52466
w2120257 <sandupama.20231385@iit.ac.lk>
2026-01-04 20:37:51 +0530

    Fix: Switch to HashRouter for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom' // <--- Changed import
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* This line below is the fix: */}
-    <BrowserRouter basename="/Estate-Agent-App-NestIn-/">
+    {/* Use HashRouter instead. It is bulletproof for GitHub Pages. */}
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
